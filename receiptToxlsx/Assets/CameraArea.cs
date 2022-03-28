8e01401e6077b9ef182a7e58eb03b51c95f452c4
서보성 <bosung.seo@gameara.com>
2022-03-28 17:26:00 +0900

    프로젝트 생성 및 카메라 켜기 구현

---
+++ receiptToxlsx/Assets/CameraArea.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraArea : MonoBehaviour
+{
+    [SerializeField] RectTransform rectTransform;
+    [SerializeField] CanvasRenderer canvasRenderer;
+    [SerializeField] RawImage rawImage;
+    [SerializeField] CameraManager camManager;
+    [SerializeField] Rect original;
+    [SerializeField] Rect CamArea;
+
+    private void Awake()
+    {
+        camManager.CamOn += ShowCamArea;
+        camManager.CamOFF += CloseCamArea;
+    }
+    private void OnDestroy()
+    {
+        camManager.CamOn -= ShowCamArea;
+        camManager.CamOFF -= CloseCamArea;
+    }
+    void ShowCamArea()
+    {
+        gameObject.active = true;
+    }
+
+    void CloseCamArea()
+    {
+        gameObject.active = false;
+    }
+
+}
---
+++ receiptToxlsx/Assets/UIInputExtension.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Ragnarok
+{
+    [AddComponentMenu("NGUI/UI/Input Field (Extension)")]
+    public sealed class UIInputExtension : UIInput
+    {
+        [SerializeField] BoxCollider2D boxCollider2D;
+
+        private bool isEnabled = false;
+
+        public System.Action OnSelection;
+
+        public void Awake()
+        {
+            boxCollider2D = GetComponent<BoxCollider2D>();
+        }
+
+        protected override void OnSelect(bool isSelected)
+        {
+            base.OnSelect(isSelected);
+
+            if (isSelected)
+                OnSelection?.Invoke();
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return isEnabled;
+            }
+            set
+            {
+                isEnabled = value;
+                boxCollider2D.enabled = isEnabled;
+            }
+        }
+    }
+}
